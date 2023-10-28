94bf6bb86e90b88a822f9703a3b83d912e450033
rustieepo0 <adityaasilwal@gmail.com>
2023-10-28 15:06:03 +0530

    Updated the timer not resetting to zero and auto resetting the text box.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 // App.js
 import React, { useState } from 'react'
 //import React from 'react';
@@ -15,7 +16,32 @@ function App() {
       <header className="App-header">
       <h3>Typing-Speed: {typingSpeed} WPM</h3>
       </header>
-      <TypingTest onTypingSpeedChange={setTypingSpeed} /> {/* Include the TypingTest component */}
+      <TypingTest onTypingSpeedChange={setTypingSpeed} /> {/* Include the TypingTest component } uncomment here
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import React, { useState } from 'react';
+import TypingTest from './TypingTest';
+import './App.css';
+
+function App() {
+  const [typingSpeed, setTypingSpeed] = useState(null);
+
+  return (
+    <div className="App">
+      <div className="NavBar">
+        <h1>KeySpeed</h1>
+      </div>
+      <div className="App-container">
+        <header className="App-header">
+          <h2>Typing Speed: {typingSpeed} WPM</h2>
+        </header>
+        <TypingTest onTypingSpeedChange={setTypingSpeed} />
+      </div>
     </div>
   );
 }
--- src/TypingTest.js
+++ src/TypingTest.js
@@ -53,6 +53,10 @@ function TypingTest({ onTypingSpeedChange }) {
 export default TypingTest;
 */
 
+/*
+
+version: 2
+
 import React, { useState, useEffect } from 'react';
 
 import './TypingTest.css';
@@ -109,6 +113,219 @@ function TypingTest({ onTypingSpeedChange }) {
       />
 
     </div> 
+  );
+}
+
+export default TypingTest;
+*/
+
+/*
+
+version: new
+import React, { useState, useEffect } from 'react';
+import './TypingTest.css';
+
+function TypingTest({ onTypingSpeedChange }) {
+  const [text, setText] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [startTime, setStartTime] = useState(null);
+
+  const startTest = () => {
+    const randomText = "This is your test text. Replace it with your own content.";
+    setText(randomText);
+    setStartTime(Date.now());
+  };
+
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    setUserInput(inputText);
+  };
+
+  useEffect(() => {
+    if (startTime) {
+      const words = userInput.trim().split(' ').length;
+      const timeInSeconds = (Date.now() - startTime) / 1000;
+      const wpm = (words / timeInSeconds) * 60;
+      onTypingSpeedChange(wpm.toFixed(2));
+    }
+  }, [userInput, startTime, onTypingSpeedChange]);
+
+  return (
+    <div className="TypingTestCenter">
+      <button onClick={startTest}>Start Typing Test</button>
+      <div className="TestText">{text}</div>
+      <input
+        type="text"
+        placeholder="Start typing here..."
+        value={userInput}
+        onChange={handleInput}
+      />
+    </div>
+  );
+}
+
+export default TypingTest;
+*/
+
+/*
+
+version
+
+import React, { useState, useEffect } from 'react';
+import './TypingTest.css';
+
+function TypingTest({ onTypingSpeedChange }) {
+  const [text, setText] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [startTime, setStartTime] = useState(null);
+
+  const startTest = () => {
+    // Reset typing speed and clear text box
+    onTypingSpeedChange('0.00');
+    setUserInput('');
+
+    //Reset start time to null
+    setStartTime(null);
+
+    const randomText = "This is your test text. Replace it with your own content.";
+    setText(randomText);
+    setStartTime(Date.now());
+  };
+
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    setUserInput(inputText);
+  };
+
+  useEffect(() => {
+    if (startTime) {
+      const words = userInput.trim().split(' ').length;
+      const timeInSeconds = (Date.now() - startTime) / 1000;
+      const wpm = (words / timeInSeconds) * 60;
+      onTypingSpeedChange(wpm.toFixed(2));
+    }
+  }, [userInput, startTime, onTypingSpeedChange]);
+
+  return (
+    <div className="TypingTestCenter">
+      <button onClick={startTest}>Start Typing Test</button>
+      <div className="TestText">{text}</div>
+      <input
+        type="text"
+        placeholder="Start typing here..."
+        value={userInput}
+        onChange={handleInput}
+      />
+    </div>
+  );
+}
+
+export default TypingTest;
+*/
+
+/*
+import React, { useState, useEffect } from 'react';
+import './TypingTest.css';
+
+function TypingTest({ onTypingSpeedChange }) {
+  const [text, setText] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+
+  const startTest = () => {
+    setUserInput('');
+    setStartTime(Date.now());
+    setEndTime(null); // Reset the end time
+    setText("This is your test text. Replace it with your own content.");
+    onTypingSpeedChange('0.00'); // Reset typing speed
+  };
+
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    setUserInput(inputText);
+
+    if (text.startsWith(inputText)) {
+      if (text === inputText) {
+        setEndTime(Date.now());
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (startTime && endTime) {
+      const words = text.trim().split(' ').length;
+      const timeInSeconds = (endTime - startTime) / 1000;
+      const wpm = (words / timeInSeconds) * 60;
+      onTypingSpeedChange(wpm.toFixed(2));
+    }
+  }, [text, startTime, endTime, onTypingSpeedChange]);
+
+  return (
+    <div className="TypingTestCenter">
+      <button onClick={startTest}>Start Typing Test</button>
+      <div className="TestText">{text}</div>
+      <input
+        type="text"
+        placeholder="Start typing here..."
+        value={userInput}
+        onChange={handleInput}
+      />
+    </div>
+  );
+}
+
+export default TypingTest;
+
+*/
+
+import React, { useState, useEffect } from 'react';
+import './TypingTest.css';
+
+function TypingTest({ onTypingSpeedChange }) {
+  const [text, setText] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [startTime, setStartTime] = useState(null);
+  const [wordsPerMinute, setWordsPerMinute] = useState('0.00');
+
+  const startTest = () => {
+    setUserInput('');
+    setStartTime(null); // Reset the start time
+    setText("This is your test text. Replace it with your own content.");
+    setWordsPerMinute('0.00');
+    onTypingSpeedChange('0.00');
+  };
+
+  const handleInput = (event) => {
+    const inputText = event.target.value;
+    setUserInput(inputText);
+
+    if (!startTime && inputText) {
+      // Start the timer when the user begins typing
+      setStartTime(Date.now());
+    }
+
+    if (text.startsWith(inputText)) {
+      const timeInSeconds = (Date.now() - startTime) / 1000;
+      const words = inputText.trim().split(' ').length;
+      const wpm = (words / timeInSeconds) * 60;
+      setWordsPerMinute(wpm.toFixed(2));
+      onTypingSpeedChange(wpm.toFixed(2));
+    }
+  };
+
+  return (
+    <div className="TypingTestCenter">
+      <button onClick={startTest}>Start Typing Test</button>
+      <div className="TestText">{text}</div>
+      <input
+        type="text"
+        placeholder="Start typing here..."
+        value={userInput}
+        onChange={handleInput}
+      />
+      {wordsPerMinute && <div>Words per minute: {wordsPerMinute}</div>}
+    </div>
   );
 }
 
